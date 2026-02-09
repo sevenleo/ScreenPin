1285bdbd079af7f31474618b91604638946d11fb
Leo Neves <enfimleo@gmail.com>
2026-02-09 04:48:44 -0300

    refactor: update paths in compile.bat to be relative and improve .gitignore

--- compile.bat
+++ compile.bat
@@ -6,12 +6,12 @@ echo [INFO] Closing all AutoHotkey instances...
 taskkill /F /IM "AutoHotkey*" /T >nul 2>&1
 taskkill /F /IM "ScreenPin.exe" /T >nul 2>&1
 
-:: Paths - Exactly as the tutorial Option A
+:: Paths - Relative to script directory
 set "COMPILER=C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe"
 set "BIN=C:\Program Files\AutoHotkey\v2\AutoHotkey64.exe"
-set "IN=D:\GITHUB\ScreenPin\ScreenPin.ahk"
-set "OUT=D:\GITHUB\ScreenPin\releases\ScreenPin.exe"
-set "ICON=D:\GITHUB\ScreenPin\icon.ico"
+set "IN=ScreenPin.ahk"
+set "OUT=releases\ScreenPin.exe"
+set "ICON=icon.ico"
 
 echo [INFO] Preparing releases folder...
 if not exist "releases" mkdir "releases"
